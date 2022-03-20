12e2dbce8710efceb2ed5dec5d68318e1293b01f
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2022-03-19 22:29:44 -0500

    first commit

---
+++ src/test/java/com/platzi/fundamentos/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.fundamentos.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
